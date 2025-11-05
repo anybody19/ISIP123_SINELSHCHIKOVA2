ecf7624c45e731dd601fb7bd4365439b037dcb1d
anybody <sinm84877@gmail.com>
2025-11-05 13:00:46 +0300

    описываем 3 основные сущности, каждая содержит поля и связи, добавляем ещё список, чтобы он не был Null

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace UniversitySystem
 {
-    internal class Program
+    // Класс Студент
+    class Student
     {
-        static void Main(string[] args)
-        {
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public List<Course> Courses { get; set; } = new List<Course>();
     }
-}
+
+    // Класс Преподавател
+    class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        public List<Course> Courses { get; set; } = new List<Course>();
+    }
+
+    // Класс Курс
+    class Course
+    {
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public Teacher Teacher { get; set; }
+
+        public List<Student> Students { get; set; } = new List<Student>();
+
