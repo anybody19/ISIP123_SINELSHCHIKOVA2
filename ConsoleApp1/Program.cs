d0ff4c2e19bb7f3cb6cac9f76578e240989a2c37
anybody <sinm84877@gmail.com>
2025-11-05 10:40:20 +0300

    создаём класс product ддобавляем общую перменную для всех товаров, добавляем нексткод для автоматического создания кода каждому товару. указываем private set, чтобы пользователь не мог менять данные о товаре

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,3 +10,14 @@ namespace StoreInventoryApp
         Electronics,
         Clothes
     }
+
+    class Product
+    {
+        private static int nextCode = 1; // Уникальный код будет увеличиваться автоматически
+
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+        public bool InStock => Quantity > 0;
+        public Category ProductCategory { get; private set; }
